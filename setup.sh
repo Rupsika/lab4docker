fc8b474726a593d97ac719624006d704d045e74d
Rupsika <rupsika77@gmail.com>
2026-04-03 16:37:22 +0530

    added setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-echo "Installing Docker..."
 sudo apt update
 sudo apt install docker.io git -y
 sudo systemctl start docker
 
-echo "Cloning Repo..."
-git clone https://github.com/Rupsika/lab4docker.git
-cd lab4docker
+echo "Docker Installed"
 
-echo "Building Image..."
-sudo docker build -t lab4 .
-
-echo "Running Container..."
-sudo docker run -d -p 3000:3000 lab4
-
-echo "Done! Open http://localhost:3000"
+docker --version
+docker run hello-world
